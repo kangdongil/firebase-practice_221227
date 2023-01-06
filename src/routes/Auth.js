2892cf1c66c326cf4ceef730d8bfa351afc95d38
Noru <akang8150@naver.com>
2023-01-06 14:17:09 +0000

    Configure Login Form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default () => <span>Auth</span>;
+export default () => {
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const onChange = (event) => {
+        const { name, value } = event.target;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+    return (
+    <div>
+        <form>
+            <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
+            <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
+            <input type="submit" value="Log In" />
+        </form>
+        <div>
+            <button>Continue with Google</button>
+            <button>Continue with GitHub</button>
+        </div>
+    </div>
+)};
